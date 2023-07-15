3a9d2e637dec9eb201cb43c238e045342b742025
Sebastian Alsina <alsinas@me.com>
2023-07-14 22:52:25 -0400

    Added Key

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
             <div className="max-w-7xl mx-auto p-12">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-12">
                     {items.map(((item, index) => {
-                        return <Item {...item} />
+                        return <Item key={item.id} {...item} />
                     }))}
                 </div>
             </div>
